deff9899f883b77f5d85e207090e786421a83113
Nicole <nscott121703@gmail.com>
2024-09-17 15:35:34 -0400

    FEAT - added classes to get play act and scene

--- play-module.js
+++ play-module.js
@@ -1,2 +1,31 @@
-/* In this module, create three classes: Play, Act, and Scene. */
+class Play {
+    constructor(data) {
+        this.Playtitle = data.title;
+        this.acts = data.acts.map(actData => new Act(actData));
+    }
 
+    returnPlay() {
+        return `<h2>${this.Playtitle}</h2>`;
+    }
+}
+
+class Act {
+    constructor(data) {
+        this.Actname = data.name;
+        this.scenes = data.scenes.map(sceneData => new Scene(sceneData));
+    }
+
+    returnAct() {
+        return `<h3>${this.Actname}</h3>`;
+    }
+}
+
+class Scene {
+    constructor(data) {
+        this.Scenename = data.name;
+    }
+
+    returnScene() {
+        return `<h4>${this.Scenename}</h4>`;
+    }
+}
