db58736329b0582cf28cf179dc0199eee9666c67
Chris Nucci <christopher.nucci@spartans.ut.edu>
2024-09-19 17:40:44 -0400

    Update play-module.js

--- play-module.js
+++ play-module.js
@@ -1,2 +1,20 @@
 /* In this module, create three classes: Play, Act, and Scene. */
+class Play {
 
+}
+
+class Act {
+    constructor(name, scenes) {
+        this.name = name;
+        this.scenes = scenes;
+    }
+}
+
+class Scene {
+    constructor(name, title, stageDirection, speeches) {
+        this.name = name;
+        this.title = title;
+        this.stageDirection = stageDirection;
+        this.speeches = speeches;
+    }
+}
